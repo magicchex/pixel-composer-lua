b3f7307c5eb00651def27033b7585f52a28d182b
magicchex <148505724+magicchex@users.noreply.github.com>
2024-10-17 12:47:55 -0400

    Rename function

--- function/get_cell_location.lua
+++ function/get_cell_location.lua
@@ -5,7 +5,7 @@
 ---@param node_width any
 ---@param node_idx any
 ---@return table
-function get_pixel_location(node_width, node_idx):
+function get_cell_location(node_width, node_idx):
     local tile_x = node_idx % node_width
     local tile_y = math.floor(node_idx / node_width)
     local height = math.ceil(node_idx / node_width)
